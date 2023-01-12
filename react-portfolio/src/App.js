ead5646907ed4d0bf485504ae13834a56b37658f
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-12 11:10:03 +0900

    ブログとホームページ専用のファイルを作成しimportを実施

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { BlogPage } from './BlogPage';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
 
 function App() {
   return (
     <div className="App">
-
       <Header/>
+      <HomePage/>
+      <BlogPage/>
     </div>
   );
 }
