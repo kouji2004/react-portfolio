a93ef32340d8fa5c28a9f43c8a2c9af7a9f2dde2
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-12 11:05:16 +0900

    headerの作成

--- react-portfolio/src/Header.js
+++ react-portfolio/src/Header.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
+  return(
+    <header>
+      <div className='logo'>
+        <h3>ポートフォリオ</h3>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>ホーム</a>
+          </li>
+          <li>
+            <a href='#'>ブログ</a>
+          </li>
+          <li>
+            <a href='#'>SNS</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
