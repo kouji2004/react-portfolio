f321b22518233ef27de6454c368e99965d3616f2
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-12 12:01:50 +0900

    ルーティングの設定

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import { BlogPage } from './BlogPage';
-import { Header } from './Header';
-import { HomePage } from './HomePage';
-import {BrowserRouter as Route, Routes} from "react-router-dom";
-
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <HomePage/>
-      <BlogPage/>
+    <div>
+      <h1>Hello React Router</h1>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
 }
 
 export default App;
