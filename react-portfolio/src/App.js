65cd94bc785f99159da6834391a402a377c2dc40
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-12 11:13:55 +0900

    import {BrowserRouter as Route, Routes} from react-router-dom;

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BlogPage } from './BlogPage';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import {BrowserRouter as Route, Routes} from "react-router-dom";
 
 function App() {
   return (
